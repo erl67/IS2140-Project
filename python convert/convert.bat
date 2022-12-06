529b0d7874e351089481f2559e59047ef12cac9f
ERL67 <erl67@pitt.edu>
2022-12-06 15:55:40 -0500

    text to json converter

---
+++ python convert/convert.bat
@@ -0,0 +1,5 @@
+ python .\convert.py -f "Conscience.txt" -t "Conscience" -a "Ames,William" -u "http://www.digitalpuritan.net/Digital%20Puritan%20Resources/Ames,%20William/Conscience.pdf"
+ 
+ python .\convert.py -f "Marrow.txt" -t "The Marrow of Sacred Divinity" -a "Ames,William" -u "http://www.digitalpuritan.net/Digital%20Puritan%20Resources/Ames,%20William/Marrow.pdf"
+ 
+ python .\convert.py -f "atreatiseonspir00colqgoog_atreatiseonspir00colqgoog.txt" -t "Treatise on spiritual comfort" -a "Colquhoun, John" -u "https://books.google.com/books/pdf/A_treatise_on_spiritual_comfort.pdf?id=qggDAAAAQAAJ&output=pdf&sig=eq7nE6ktwE15S3OZVtiIh5uFLic"
---
+++ python convert/convert.py
@@ -0,0 +1,75 @@
+import sys, argparse, json, re
+
+# clear; python .\convert.py -f "Conscience.txt" -t "Conscience" -a "Ames,William" -u "http://www.digitalpuritan.net/Digital%20Puritan%20Resources/Ames,%20William/Conscience.pdf"
+stopwords = []
+puncRE = '[^\w\s]'
+
+def get_args(argv):
+    parser = argparse.ArgumentParser(description="Library Book Converter. Text to JSON.")
+    parser.add_argument('-f', '--file', required=True, type=str)
+    parser.add_argument('-t', '--title', required=True, type=str)
+    parser.add_argument('-a', '--author', required=True, type=str)
+    parser.add_argument('-u', '--url', required=True, default="Unavailable", type=str)
+    return parser.parse_args()
+
+
+def isStopword(word):
+    global stopwords
+    if word in stopwords or len(word) <= 2:
+        return True
+    return False
+
+
+def convert_file(f, t, a, u):
+    dictionary = {
+    "title": t,
+    "author": a,
+    "file": f,
+    "url": u,
+    "content" : ""
+    }
+
+    content = ""
+    tempContent = []
+    temp = ""
+    file = open("./text/" + f, 'rt', encoding="utf-8")
+
+    while temp := file.readline():
+        temp = temp.strip()
+        # print(f"{temp=}")
+        if temp == '':
+            continue
+
+        #remove punctuation
+        words = re.sub(puncRE, "", temp).split(" ")
+        # print(f"{words=}")
+
+        for word in words:
+            # print(f"{word=}")
+            if not isStopword(word):
+                tempContent.append(word)
+
+    file.close()
+    # print(f"{tempContent=}")
+    content = ' '.join(tempContent)
+    print(f"{content=}")
+
+    dictionary["content"] = content
+
+    json_object = json.dumps(dictionary, indent=4)
+    with open("./json/" + a + "_" + t + ".json", "w") as file:
+        file.write(json_object)
+
+
+def main(argv):
+    args = get_args(argv)
+    with open("./stopword.txt", 'r', encoding="ANSI") as file:
+        stopwords = file.read().split()
+    if not args.file:
+        print("Command line arguments must be supplied\n")
+    else:
+        convert_file(args.file, args.title, args.author, args.url)
+
+ 
+if __name__ == "__main__":
+    main(sys.argv[1:])
